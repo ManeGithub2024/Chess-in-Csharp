bb299ce0b948d6a0f9c28c4c30000a2148daa555
MANE <nechainn@gmail.com>
2025-04-20 15:17:24 +0300

    delete redundant console colors

--- ConsoleApp/BoardCellSprite.cs
+++ ConsoleApp/BoardCellSprite.cs
@@ -13,7 +13,6 @@
         // ANSI escape codes for text colors
         public const string BlackText = "\u001B[34m";
         public const string WhiteText = "\u001B[97m";
-        public const string GrayText = "\u001B[90m";
 
         public Dictionary<ConsoleColor, string> BackgroundColors { get; private set; }
         public Dictionary<ConsoleColor, string> FontColors { get; private set; }
@@ -23,15 +22,13 @@
             BackgroundColors = new Dictionary<ConsoleColor, string> {
                 { ConsoleColor.ResetColor, ResetColor},
                 { ConsoleColor.White, WhiteBackground},
-                { ConsoleColor.Black, BlackBackground},
-                { ConsoleColor.Gray, BlackText}                
+                { ConsoleColor.Black, BlackBackground}               
             };
 
             FontColors = new Dictionary<ConsoleColor, string> {
                 { ConsoleColor.ResetColor, ResetColor},
                 { ConsoleColor.White, WhiteText},
-                { ConsoleColor.Black, BlackText},
-                { ConsoleColor.Gray, GrayText}
+                { ConsoleColor.Black, BlackText}
             };
         }
     }
--- ConsoleApp/Color.cs
+++ ConsoleApp/Color.cs
@@ -4,7 +4,6 @@
     {
         White = 1,
         Black,
-        Gray,
         ResetColor
     }
 }
