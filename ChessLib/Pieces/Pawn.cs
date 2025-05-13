d724bc49deb669b5d0fa937923f003623a040d0d
MANE <nechainn@gmail.com>
2025-05-13 03:22:30 +0300

    add pawn move logic

--- ChessLib/Pieces/Pawn.cs
+++ ChessLib/Pieces/Pawn.cs
@@ -1,4 +1,6 @@
-﻿namespace ChessLib.Pieces
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ChessLib.Pieces
 {
     public class Pawn : Piece
     {
@@ -9,12 +11,43 @@
 
         protected override HashSet<CoordinatesShift> GetPieceShift()
         {
-            throw new NotImplementedException();
+            var shifts = new HashSet<CoordinatesShift>();
+            if (this.Color == Color.White) {
+                shifts.Add(new CoordinatesShift(0, 1)); // 1 cell up                
+                if (this.Coordinates.Rank == Rank.Rank2) {
+                    shifts.Add(new CoordinatesShift(0, 2)); // 2 celss up
+                }
+
+                // for attack
+                shifts.Add(new CoordinatesShift(-1, 1));
+                shifts.Add(new CoordinatesShift(1, 1));
+            }
+
+            if (this.Color == Color.Black) {
+                shifts.Add(new CoordinatesShift(0, -1)); // 1 cell down
+                if (this.Coordinates.Rank == Rank.Rank7) {
+                    shifts.Add(new CoordinatesShift(0, -2)); // 2 cells down
+                }
+
+                // for attack
+                shifts.Add(new CoordinatesShift(-1, -1));
+                shifts.Add(new CoordinatesShift(1, -1));
+            }
+
+            return shifts;
         }
 
         protected override bool IsCellAvailableForMove(Coordinates target, Board board)
         {
-            throw new NotImplementedException();
+            if (this.Coordinates.File != target.File && board.IsCellEmpty(target)) {
+                return false;
+            }
+
+            if (this.Coordinates.File == target.File && !board.IsCellEmpty(target)) {
+                return false;
+            }
+
+            return base.IsCellAvailableForMove(target, board);
         }
     }
 }
